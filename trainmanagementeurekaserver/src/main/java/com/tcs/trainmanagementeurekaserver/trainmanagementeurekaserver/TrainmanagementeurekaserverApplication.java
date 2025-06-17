aec465691ea10c6a90e6bd8c22c949b932960f13
Harshit Khandelwal <86138193+Harshit2756@users.noreply.github.com>
2025-06-18 02:13:54 +0530

    Add initial microservices architecture for Train Management System
    - Created services for Train Management, Booking, Food, Customer Support, and User Management.
    - Added service registry using Eureka Server.
    - Implemented API Gateway for routing requests.
    - Established user registration and authentication processes.
    - Included necessary configuration files and project structure for each service.
    - Added Maven wrapper and configuration files for build management.
    - Implemented basic application properties and test classes for each service.

---
+++ trainmanagementeurekaserver/src/main/java/com/tcs/trainmanagementeurekaserver/trainmanagementeurekaserver/TrainmanagementeurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.trainmanagementeurekaserver.trainmanagementeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainmanagementeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainmanagementeurekaserverApplication.class, args);
+	}
+
+}
---
+++ trainmanagementeurekaserver/src/test/java/com/tcs/trainmanagementeurekaserver/trainmanagementeurekaserver/TrainmanagementeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.trainmanagementeurekaserver.trainmanagementeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainmanagementeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
